a53afa6e54299645caeab0435d6de7fe2b457f49
Padilhovisck <eduardo.padilha@gmail.com>
2018-02-14 17:37:25 -0200

    Teste de alteração
    Arquivo deletado

---
+++ client.py
@@ -0,0 +1,16 @@
+import requests
+import base64
+
+with open("C:\\Users\\Eduardo\\PycharmProjects\\vepynet\\PDF\\Aditivo.pdf", "rb") as pdf_file:
+    encoded_string = base64.b64encode(pdf_file.read())
+
+r = requests.post('http://localhost:5000/api/values',
+                  json={"NumeroDoLote": 10522,
+                        "Cliente": 'ZFAC FOMENTO MERCANTIL LTDA',
+                        "Cedente": 'OUROLAC IND. BRASILEIRA ME',
+                        "Filename": encoded_string.decode('utf-8')
+                        }
+                  )
+print(r.status_code)
+print(r.json())
+
